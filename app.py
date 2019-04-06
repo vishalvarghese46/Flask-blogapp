45070cfd8e1e57bc848b5eac4d4048427ceead6d
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-04-06 19:09:20 +0100

    Rendering templates, adding bootstrap on home and about pages

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+data = [
+    {
+        'author': 'Vishal Varghese',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 06, 2019',
+     },
+{
+        'author': 'Ben Philip',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 01, 2019',
+     },
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=data)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
